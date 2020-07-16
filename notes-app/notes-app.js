7477657fef648606de6101c8f20e54f410074a6a
santhosh.pemmaka <santhosh.pemmaka@gmail.com>
2020-07-16 18:02:05 +0530

    client side interaction opertion through js

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,17 +1,82 @@
 // DOM - Document object model
+const note1 = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spin'
+
+},
+{   
+    title: 'habbitis to  work on',
+    body : "execrise, Eating, Sleeping"
+
+},
+{
+    title: "to workout",
+    body: "to ruuning daily"
+},{
+    title : "watch movie",
+    body : "action movies "
+}]
+
+const filters = {
+    searchText: ''
+}
+
+const render = (notes,filters) =>{
+    const filterNotes = notes.filter((note) => {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+
+    })
+    
+    document.querySelector('#notes').innerHTML = ''
+
+    filterNotes.forEach((filter) => {
+        const noteEl = document.createElement('p')
+        noteEl.textContent = filter.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+render(note1,filters)
+
+document.querySelector('#createNote').addEventListener('click', (e) =>{
+    e.target.textContent = "The button Clicked"
+})
+
+
+document.querySelector('#inputText').addEventListener('input',(e) => {
+    filters.searchText = e.target.value;
+    render(note1,filters)
+})
+
+document.querySelector("#filterId").addEventListener('change',(e) => {
+    console.log(e.target.value)
+})
+
+// document.querySelector('#name-form').addEventListener('submit',(e) => {
+//     e.preventDefault()
+//     console.log(e.target.elements.firstName.value)
+//     e.target.elements.firstName.value = ''
+// })
+
+
 
 // query remove
-const param = document.querySelector('p')
-param.remove()
+// const param = document.querySelector('p')
+// param.remove()
 
-// query all and remove
+// // query all and remove
 
-const params = document.querySelectorAll('p')
+// const params = document.querySelectorAll('p')
 
-params.forEach((par) =>{
-    par.textContent ="This is overwritten text"
-    console.log(par.textContent)
-    par.remove()
-})
+// params.forEach((par) =>{
+//     par.textContent ="This is overwritten text"
+//     // console.log(par.textContent)
+//     // par.remove()
+// })
+
+// const newParagraphy = document.createElement('p')
+// newParagraphy.textContent = "This is the new element from javaScript"
+// document.querySelector('body').appendChild(newParagraphy)
+
 
 
--- todo-app/todoList.js
+++ todo-app/todoList.js
@@ -1,9 +1,103 @@
-params = document.querySelectorAll('p');
+const todos = [{
+    text:"order dinner",
+    completed: false
+},
+{
+    text:"Go to the movie",
+    completed: true
+},
+{
+    text:"Watching the tv show",
+    completed: false
+},
+{
+    text:"Learn the JavaScript",
+    completed: false
+},
+{
+    text:"Go to the outing",
+    completed: true
+},
+{
+    text:"Eat the breakfast",
+    completed: false
+}]
 
-params.forEach((para) => {
-    let result_para = para.textContent.includes('the')
-    if(result_para){
-        para.remove()
-    }
+let filters = {
+    searchText : ''
+}
+
+const renderTodos = (todos,filters) => {
+    const newTodo = todos.filter((todo) => {
+        return todo.text.toLowerCase().includes(filters.searchText)
+    })
+
+    document.querySelector("#todos").innerHTML = ''
+    let todo_left = 0
+    todos.forEach((todo) => {
+        if(todo.completed == false){
+            todo_left++;
+        }
+    })
+
+    const newParaTodo = document.createElement('h2')
+    newParaTodo.textContent = `You have ${todo_left} todos left`
+    document.querySelector('#todos').appendChild(newParaTodo)
+    newTodo.forEach((todo) => {
+        const newPara = document.createElement('p')
+        newPara.textContent = todo.text
+        document.querySelector('#todos').appendChild(newPara)
+    })
+
+
+}
+
+renderTodos(todos,filters)
+
+document.querySelector('#todo-list').addEventListener('submit',(e) => {
+    e.preventDefault()
+    todos.push({
+        text : e.target.elements.todoList.value,
+        completed: false
+    })
+    renderTodos(todos,filters)
+    e.target.elements.todoList.value = ''
 })
+
+document.querySelector("#inputTodo").addEventListener('input',(e) => {
+    filters.searchText = e.target.value
+    renderTodos(todos,filters)
+})
+
+document.querySelector("#todoList").addEventListener('change',(e) => {
+    if(e.target.checked){
+        const newTodo = todos.filter((todo) => {
+            return !todo.completed == e.target.checked
+        })
+        renderTodos(newTodo,filters)
+    }
+    else{
+        renderTodos(todos,filters)
+    } 
+    
+    
+})
+
+
+// adding button addlistener
+
+// document.querySelector('#addTodo').addEventListener('click',(e) => {
+//     console.log("adding  to the todo...")
+// })
+
+// // input todo 
+
+// params = document.querySelectorAll('p');
+
+// params.forEach((para) => {
+//     let result_para = para.textContent.includes('the')
+//     if(result_para){
+//         para.remove()
+//     }
+// })
 
